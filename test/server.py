ed8f6fbc3ca55c56aafdaf91e72814f211cf6a2c
David Wolever <david@wolever.net>
2011-08-14 11:38:02 -0400

    Updating server side of the testing framework

--- test/server.py
+++ test/server.py
@@ -1,7 +1,48 @@
 import os
+import re
 import json
 
 from werkzeug.wrappers import Request, Response
+
+class AppError(Exception):
+    def __init__(self, msg, data):
+        self.msg = msg
+        self.data = data
+
+
+class TestAPI(object):
+    def echo(self, *args, **kwargs):
+        result = dict(kwargs)
+        result.update([ (idx, var) for (idx, var)
+                        in enumerate(args) ])
+        return result
+
+
+def handle_rpc(request, func_name, api):
+    api.get_kwargs = dict(request.args.items())
+    api.post_kwargs = dict(request.form.items())
+
+    kwargs = dict(api.get_kwargs)
+    kwargs.update(api.post_kwargs)
+
+    api.json_kwargs = json.loads(kwargs.pop("__kwargs", "{}"))
+    kwargs.update(api.json_kwargs)
+
+    args = json.loads(kwargs.pop("__args", "[]"))
+
+    try:
+        func = getattr(api, func_name)
+        func_result = func(*args, **kwargs)
+        result = { "ok": True, "data": func_result }
+    except AppError, e:
+        result = {
+            "error": True,
+            "data": e.data,
+            "msg": e.msg,
+        }
+
+    return Response(json.dumps(result))
+
 
 @Request.application
 def application(request):
@@ -9,13 +50,11 @@ def application(request):
         return Response(status=302, headers=[
             ("Location", "/static/test/test.html"),
         ])
-    if request.path.startswith("/echo/"):
-        result = dict(request.args.items())
-        result.update(json.loads(result.pop("__kwargs", "{}")))
-        args = json.loads(result.pop("__args", "[]"))
-        result.update([ (idx, val) for (idx, val)
-                         in enumerate(args) ])
-        return Response(json.dumps({ "ok": True, "data": result }))
+
+    match = re.match("^/api/(.+)", request.path)
+    if match:
+        api = TestAPI()
+        return handle_rpc(request, match.group(1), api)
     return Response("unknown: " + request.path, status=404)
 
 if __name__ == '__main__':
